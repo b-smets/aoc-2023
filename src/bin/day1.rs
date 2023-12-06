892b58297686373a739a7cfc642b26818b03a705
bsmets <bart.smets@unifiedpost.com>
2023-12-06 22:27:51 +0100

    Implemented AOC 2023 day 1 part 2.
    Fetch data and submit result using the aocd crate. Only use regexes for substituting textual numbers.

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -3,16 +3,16 @@ use aocd::prelude::*;
 use lazy_regex::regex_replace_all;
 use std::str::Lines;
 
-const TEXT_TO_NUMBER: Map<&'static str, char> = phf_map! {
-    "one" => '1',
-    "two" => '2',
-    "three" => '3',
-    "four" => '4',
-    "five" => '5',
-    "six" => '6',
-    "seven" => '7',
-    "eight" => '8',
-    "nine" => '9',
+const TEXT_TO_NUMBER: Map<&'static str, &'static str> = phf_map! {
+    "one" => "o1e",
+    "two" => "t2o",
+    "three" => "t3e",
+    "four" => "f4r",
+    "five" => "f5e",
+    "six" => "s6x",
+    "seven" => "s7n",
+    "eight" => "e8t",
+    "nine" => "n9e",
 };
 
 fn get_replacement(textual_number: &str) -> String {
@@ -20,16 +20,23 @@ fn get_replacement(textual_number: &str) -> String {
 }
 
 fn substitute_textual_numbers(str: &str) -> String {
-    regex_replace_all!(
-        r"(one|two|three|four|five|six|seven|eight|nine)",
-        str,
-        |_, textual_numeric| get_replacement(textual_numeric)
-    )
-    .to_string()
-}
+    let mut previous_replaced_string = str.to_owned();
 
-fn is_numeric(ch: char) -> bool {
-    return ch >= '0' && ch <= '9';
+    loop {
+        let replaced_string = regex_replace_all!(
+            r"(one|two|three|four|five|six|seven|eight|nine)",
+            &previous_replaced_string,
+            |_, textual_numeric| { get_replacement(textual_numeric) }
+        );
+
+        if replaced_string == previous_replaced_string {
+            break;
+        }
+
+        previous_replaced_string = replaced_string.to_string();
+    }
+
+    previous_replaced_string.to_string()
 }
 
 fn parse_numbers(input: &str, support_textual_numbers: bool) -> u32 {
@@ -39,38 +46,34 @@ fn parse_numbers(input: &str, support_textual_numbers: bool) -> u32 {
         input.to_string()
     };
 
-    let first_digit_index = processed_input.find(|c| is_numeric(c)).unwrap();
-    let last_digit_index = processed_input.rfind(|c| is_numeric(c)).unwrap();
-
-    let first_digit = processed_input.chars().nth(first_digit_index).unwrap();
-    let last_digit = processed_input.chars().nth(last_digit_index).unwrap();
-
-    let result = format!("{}{}", first_digit, last_digit)
-        .parse::<u32>()
+    let first_digit = processed_input
+        .chars()
+        .find(|ch: &char| ch.is_digit(10))
+        .unwrap();
+    let last_digit = processed_input
+        .chars()
+        .rev()
+        .find(|ch: &char| ch.is_digit(10))
         .unwrap();
 
-    println!("{} - {}{} - {}", input, first_digit, last_digit, result);
-    result
+    format!("{}{}", first_digit, last_digit)
+        .parse::<u32>()
+        .unwrap()
 }
 
 fn run(input: Lines, textual_numbers: bool) -> u32 {
     input
         .map(|line| parse_numbers(line, textual_numbers))
-        .reduce(|acc, current| {
-            println!("Adding {} {} => {}", acc, current, acc + current);
-
-            acc + current
-        })
-        .unwrap()
+        .sum::<u32>()
 }
 
 #[aocd(2023, 1)]
 fn main() {
     let calibration_input = input!();
 
-    // let result_part1 = run(calibration_input.lines(), false);
+    let result_part1 = run(calibration_input.lines(), false);
     let result_part2 = run(calibration_input.lines(), true);
 
-    // submit!(1, result_part1);
+    submit!(1, result_part1);
     submit!(2, result_part2);
 }
